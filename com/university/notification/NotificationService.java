6b31123195995b08a07d0d9a675d82aec76cd8ef
Hassan Ahmed <hassan.alhardlow@gmail.com>
2026-01-18 13:58:22 +0200

    Add NotificationService interface for notifications

---
+++ com/university/notification/NotificationService.java
@@ -0,0 +1,9 @@
+package com.university.notification;
+
+/**
+ * Notification Service Interface
+ * This demonstrates Abstraction as required by the project.
+ */
+public interface NotificationService {
+    void sendNotification(String recipient, String message);
+}
