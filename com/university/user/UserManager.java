410cdf81e1f42b5a5d0037010ed69b7e0d053099
Junior Mayila <mayilasandzafortunejunior@gmail.com>
2026-01-18 17:56:00 -0800

    Add UserManager.java class (Component 1)

---
+++ com/university/user/UserManager.java
@@ -0,0 +1,48 @@
+package com.university.services;
+
+import com.university.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+
+    private List<User> users;
+
+    public UserManager() {
+        users = new ArrayList<>();
+    }
+
+    /**
+     * Add a new user to the system
+     */
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    /**
+     * Get a user by ID
+     */
+    public User getUserById(String userId) {
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get all users
+     */
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users);
+    }
+
+    /**
+     * Delete a user by ID
+     */
+    public boolean deleteUser(String userId) {
+        return users.removeIf(user -> user.getUserId().equals(userId));
+    }
+}
